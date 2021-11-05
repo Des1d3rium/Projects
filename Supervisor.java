01d2c83de24fba467befa0bc898daf0a975b81a3
AinfinitedZ <daniel_sheep@163.com>
2021-11-05 09:59:03 -0400

    supervisor defined

---
+++ Supervisor.java
@@ -0,0 +1,13 @@
+/**
+ * this abstract class explore common traits of all three kinds of supervisor
+ */
+
+public abstract class Supervisor
+{
+    public abstract String getFirstName();
+
+    public abstract void setName(String firstName, String lastName);
+    
+    public abstract String getNumber();
+
+}
