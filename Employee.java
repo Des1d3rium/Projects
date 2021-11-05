d9071aaab0d7fe3ac2024fe4cbda9c20a151d2ae
AinfinitedZ <daniel_sheep@163.com>
2021-11-05 10:13:08 -0400

    111

--- Employee.java
+++ Employee.java
@@ -1,5 +1,6 @@
 /**
  * this abstract class explore common traits of all three kinds of employee.
+ * @author dxl746
  */
 public abstract class Employee
 {
@@ -20,7 +21,9 @@ public abstract class Employee
     public abstract void adjustPay(double percentage);
 
     public abstract boolean equals(Employee employee1, Employee employee2);
-    
+
+    public abstract String toString(); 
+
     public abstract int compareToByName(String name);
 
     public abstract int compareToByEarning(double earning);
