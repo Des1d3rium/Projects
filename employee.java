9b2f2b2468ecb146190a9fcdff3b064fc788d953
AinfinitedZ_Surface <daniel_sheep@163.com>
2021-11-05 07:48:38 -0400

    1

--- employee.java
+++ employee.java
@@ -5,4 +5,23 @@ public abstract class employee
 {
     public abstract String getFirstName();
     
+    public abstract String getLastName();
+    
+    public abstract void setName(String firstName, String lastName);
+    
+    public abstract String getNumber();
+
+    public abstract double getBonus();
+
+    public abstract void setBonus(double bonus);
+
+    public abstract double getAmountEarned();
+
+    public abstract void adjustPay(double percentage);
+
+    public abstract boolean equals(employee employee1, employee employee2);
+    
+    public abstract int compareToByName(String name);
+
+    public abstract int compareToByEarning(double earning);
 } 
