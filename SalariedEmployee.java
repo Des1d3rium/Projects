456483935218dfc35e06faae9b5e28d7a5bb0d87
AinfinitedZ_Surface <daniel_sheep@163.com>
2021-11-06 17:06:45 -0400

    111

--- SalariedEmployee.java
+++ SalariedEmployee.java
@@ -1,16 +1,34 @@
 /**
- * this class contains behaviours of a salaried employee
+ * A <code>SalariedEmployee</code> class represent employee paid by fixed salary. 
+ * In order to create a<code>SalariedEmployee</code>, one's 
  * @author dxl746
  */
 public class SalariedEmployee extends Employee
 {
+    // this field stores the first name of employee
     private String firstName;
+
+    // this field stores the last name of employee
     private String lastName;
+
+    // this field stores the serial number of the employee
     private String number;
+
+    // this field stores the salary of the employee
     private double salary;
+
+    // this field stores the bouns of the employee
     private double bonus;
+
+    // this field stores the supervisor of the employee
     private Supervisor supervisor;
 
+    /**
+     * @param firstNameInput The first name of employee
+     * @param lastNameInput The last name of employee
+     * @param numberInput The serial number of employee
+     * @param salaryInput 
+     */
     public SalariedEmployee(String firstNameInput, String lastNameInput, String numberInput, double salaryInput)
     {
         this.firstName = firstNameInput;
@@ -19,63 +37,115 @@ public class SalariedEmployee extends Employee
         this.salary = salaryInput; 
     }
 
+
+    
+    /** 
+     * @return <code>firstName</code> The first name of employee
+     */
     public String getFirstName()
     {
         return firstName;
     }
 
+    /**
+     * @return <b>lastName</b> The last name of employee
+     */
     public String getLastName()
     {
         return lastName;
     }
 
+
+    
+    /** 
+     * @param firstNameInput The first name 
+     * @param lastNameInput The last name 
+     */
     public void setName(String firstNameInput, String lastNameInput)
     {
         firstName = firstNameInput;
         lastName = lastNameInput;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getNumber()
     {
         return number;
     }
 
+    
+    /** 
+     * @return double
+     */
     public double getSalary()
     {
         return salary;
     }
 
+    
+    /** 
+     * @param salaryInput
+     */
     public void setSalary(double salaryInput)
     {
         salary = salaryInput;
     }
 
+    
+    /** 
+     * @return double
+     */
     public double getBonus()
     {
         return bonus;
     }
 
+    
+    /** 
+     * @param bonusInput
+     */
     public void setBonus(double bonusInput)
     {
         bonus = bonusInput;
     }
 
+    
+    /** 
+     * @return double
+     */
     public double getAmountEarned()
     {
         return getBonus()+getSalary();
     }
 
+    
+    /** 
+     * @param percentage
+     */
     public void adjustPay(double percentage)
     {
         double salaryTemp = this.getSalary() * (1.0+percentage);
         this.setSalary(salaryTemp);
     }
 
+    
+    /** 
+     * @return String
+     */
     public String toString()
     {
         return getNumber() + ": " + getLastName() + ", " + getFirstName() + ", Salaried Employee";
     }
 
+    
+    /** 
+     * @param employeeInput1
+     * @param employeeInput2
+     * @return boolean
+     */
     public boolean equals(Employee employeeInput1, Employee employeeInput2)
     {
         if(employeeInput1.getFirstName().equals(employeeInput2.getFirstName()))
@@ -91,6 +161,11 @@ public class SalariedEmployee extends Employee
         return false;
     }
 
+    
+    /** 
+     * @param employeeInput
+     * @return int
+     */
     public int compareToByName(Employee employeeInput)
     {
         if(this.getLastName().compareToIgnoreCase(employeeInput.getLastName()) == 0)
@@ -103,16 +178,29 @@ public class SalariedEmployee extends Employee
         }
     }
     
+    
+    /** 
+     * @param employeeInput
+     * @return int
+     */
     public int compareToByEarning(Employee employeeInput)
     {
         return (int)(this.getAmountEarned() - employeeInput.getAmountEarned()); 
     } 
 
+    
+    /** 
+     * @param supervisorInput
+     */
     public void setSupervisor(Supervisor supervisorInput)
     {
         supervisor = supervisorInput;
     }
 
+    
+    /** 
+     * @return Supervisor
+     */
     public Supervisor getSupervisor()
     {
         return supervisor;
